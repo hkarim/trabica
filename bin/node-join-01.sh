774ef9bea9d7fd7839a5874e0dff0c1fb3303482
hk <hossam.karim@gmail.com>
2023-04-24 08:37:49 +0200

    cluster setup

---
+++ bin/node-join-01.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+mod/node-template/target/universal/stage/bin/node-template -- \
+join \
+--ip 0.0.0.0 \
+--port 6001 \
+--peer-address 127.0.0.1:5555
--- bin/node-join-02.sh
+++ bin/node-join-02.sh
@@ -2,5 +2,5 @@
 mod/node-template/target/universal/stage/bin/node-template -- \
 join \
 --ip 0.0.0.0 \
---port 6666 \
+--port 6002 \
 --peer-address 127.0.0.1:5555
